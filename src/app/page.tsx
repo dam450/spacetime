009445782ec1d78b428a90bb268c2e4eb816384c
Dam450 <68165752+dam450@users.noreply.github.com>
2023-07-04 10:36:41 -0300

    feat: create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,79 @@
+import Image from 'next/image'
+import { User } from 'lucide-react'
+
+import nlwLogo from '../assets/nlw-spacetime-logo.svg'
+
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
-      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
-    </div>
+    <main className="grid min-h-screen grid-cols-2">
+      {/* left panel */}
+      <div className="relative flex flex-1 flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+        {/* blur */}
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+        {/* stripes */}
+        <div className="absolute bottom-0 right-1 top-0 w-2  bg-stripes" />
+
+        <a
+          href=""
+          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
+        >
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+          <p className="max-w-[140px] text-sm leading-snug ">
+            <span className="underline">Crie sua conta</span> e salve suas
+            memórias!
+          </p>
+        </a>
+
+        {/* hero */}
+        <div className="space-y-5">
+          <Image src={nlwLogo} alt="NLW Spacetime" />
+          <div className="max-w-[420px] space-y-1">
+            <h1 className="text-5xl font-bold leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </h1>
+            <p className=" text-lg leading-relaxed">
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </div>
+          <a
+            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black transition-transform hover:scale-105 hover:bg-green-700"
+            href=""
+          >
+            Cadastrar lembrança
+          </a>
+        </div>
+
+        {/* copyright */}
+        <div>
+          <p className="text-sm leading-relaxed">
+            Feito com 💜 no NLW da{' '}
+            <a
+              className="transition-colors hover:text-gray-50 hover:underline"
+              href="https://rocketseat.com.br"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Rocketseat
+            </a>
+          </p>
+        </div>
+      </div>
+
+      {/* right panel */}
+      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="max-w-[360px] text-center text-base leading-relaxed text-gray-100">
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a className="underline hover:text-gray-50" href="#">
+              criar agora
+            </a>
+            !
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
