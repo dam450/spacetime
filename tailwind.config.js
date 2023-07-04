268afa89fc75c31c0288d5434d509879c8fce46a
Dam450 <68165752+dam450@users.noreply.github.com>
2023-07-04 10:36:10 -0300

    chore: set tailwind config
    extends custom keys

--- tailwind.config.js
+++ tailwind.config.js
@@ -74,6 +74,22 @@ module.exports = {
           900: '#6b501b',
         },
       },
+
+      backgroundImage: {
+        stripes:
+          'linear-gradient(to bottom, rgba(255,255,255,0.1),rgba(255,255,255,0.1) 12.5%, transparent 12.5%, transparent 100% )',
+      },
+      backgroundSize: {
+        stripes: '100% 8px',
+      },
+
+      blur: {
+        full: '194px',
+      },
+
+      fontSize: {
+        '5xl': '2.5rem',
+      },
     },
   },
   plugins: [],
