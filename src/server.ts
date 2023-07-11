454679026bdf5ab88b576401e2a0f4199250838e
Dam450 <68165752+dam450@users.noreply.github.com>
2023-07-11 10:50:07 -0300

    chore: ♻️add routes & dependencies to server

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,35 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
-import { memoriesRoutes } from './routes/memories'
+import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 
+import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
+
+const port = Number(process.env.SERVER_PORT) || 3000
+const jwtConfig = { secret: process.env.JWT_SECRET }
 const app = fastify()
-const port = 3333
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
 })
 
+app.register(jwt, jwtConfig)
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 
 app.get('/', async (req, res) => {
   res.status(200).send({ server: 'online' })
