42269491495b5533f018873c48e3f9eb5346cc19
Dam450 <68165752+dam450@users.noreply.github.com>
2023-07-11 10:56:10 -0300

    refactor: 📦move fixed elements to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
 import { ReactNode } from 'react'
+import { cookies } from 'next/headers'
 import './globals.css'
 
 import {
   Roboto_Flex as Roboto,
   Bai_Jamjuree as Jamjuree,
 } from 'next/font/google'
+
+import { Copyright } from '@/components/Copyright'
+import { Hero } from '@/components/Hero'
+import { Profile } from '@/components/Profile'
+import { SignIn } from '@/components/SignIn'
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
 
@@ -20,12 +26,37 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <html lang="pt-BR">
       <body
         className={`${roboto.variable} ${jamjuree.variable} bg-gray-900 font-sans text-gray-100 `}
       >
-        {children}
+        <main className="grid min-h-screen grid-cols-2">
+          {/* left panel */}
+          <div className="relative flex flex-1 flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+            {/* blur */}
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+            {/* stripes */}
+            <div className="absolute bottom-0 right-1 top-0 w-2  bg-stripes" />
+
+            {isAuthenticated ? (
+              <Profile key={'authenticated'} />
+            ) : (
+              <SignIn key={'unauthenticated'} />
+            )}
+
+            <Hero />
+
+            <Copyright />
+          </div>
+
+          {/* right panel */}
+          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   )
